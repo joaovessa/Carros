815507f9bb430de3e79ba221c7c8d7e57fc9a134
João Vitor Silva <jvsa.110507@gmail.com>
2024-03-28 07:56:19 -0300

    terceiro commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ export default function app() {
   return(
   <main>
     <section>
-      <div>
+      <div className="bg-orange">
         <img src={iconSedans} alt="icone dos carros sedans" />
         <h2>Sedans</h2>
         <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
         <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
       </div>
-      <div>
+      <div className="bg-green">
         <img src={iconSuvs} alt="icone dos carros suvs" />
         <h2>Suvs</h2>
         <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
         <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
       </div>
-      <div>
+      <div className="bg-dark-green">
         <img src={iconLuxury} alt="icone dos carros de luxo" />
         <h2>Luxuosos</h2>
         <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
